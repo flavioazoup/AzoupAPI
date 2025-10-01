e2d3284a8084c2763ecf32d40d12bbab23f38c17
flavio@azoup.com.br <flavio@azoup.com.br>
2025-10-01 20:54:39 -0300

    ultimos ajustes

--- app/core/auth_database.py
+++ app/core/auth_database.py
@@ -2,13 +2,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_AUTH_DATABASE_URL = "firebird+fdb://SYSDBA:ZPFs1st3m4s@18.228.48.187:3050//opt/firebird/data/AZOUPFB5.FDB"
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+SQLALCHEMY_AUTH_DATABASE_URL = os.getenv("AUTH_DB_URL")
 
 auth_engine = create_engine(SQLALCHEMY_AUTH_DATABASE_URL, echo=True)
 AuthSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=auth_engine)
 
 def get_auth_db():
+    """
+    Retorna uma sessão do banco de autenticação, usando a string de conexão do .env
+    """
     db = AuthSessionLocal()
     try:
         yield db
